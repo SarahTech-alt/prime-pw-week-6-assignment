63ae9b266751d8a0b293cddd51710312d19323fd
Kris Szafranski <kris@thoughtshelter.com>
2021-07-13 15:22:24 -0500

    added hint to use this function directions

--- assignment/scripts/client.js
+++ assignment/scripts/client.js
@@ -2,6 +2,7 @@ let garage = [];
 
 /*
 Do not change newCar for base mode!
+HINT: You will need to gather the input values and then call this function, passing in those input values.
 */
 function newCar(yearInput, makeInput, modelInput){
   console.log('in newCar:', yearInput, makeInput, modelInput);
