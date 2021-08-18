268dc57500c02382ecc1a464e695ce8811800a17
Sarah Fuoss <sfuoss@gmail.com>
2021-08-18 10:44:36 -0500

    reset inputs after function run

--- assignment/scripts/client.js
+++ assignment/scripts/client.js
@@ -15,17 +15,33 @@ function newCar(yearInput, makeInput, modelInput){
     model: modelInput
   };
   garage.push(newCarObject);
-  let garageOutput = $( "#garageList" );
-  garageOutput.append(`<li> ${yearInput} ${makeInput} ${modelInput} </li>`);
+  // let garageOutput = $( "#garageList" );
+  // garageOutput.append(`<li> ${yearInput} ${makeInput} ${modelInput} </li>`);
   return true;
 } // end newCar
+
 
 function ready(){
   $("#addButton").on('click', assign );
 }
 function assign(){
+  // placing user input into variables to work with
   let yearInput = $("#yearInput").val();
   let makeInput = $("#makeInput").val();
   let modelInput = $("#modelInput").val();
+  // running the newCar function to add items to garage
   newCar(yearInput,makeInput,modelInput);
+  // add car input to DOM
+  $("#garageList").append(`<li> ${yearInput} ${makeInput} ${modelInput}`);
+  // clear input values
+  $("#yearInput").val('');
+  $("#makeInput").val('');
+  $('#modelInput').val('');
 }
+
+// if (newCar()){
+//   //for (let cars in garage){
+//     console.log(garage);
+//     //$("#garageList").append(`<li> ${cars.yearInput}`))
+//   //}
+// }
