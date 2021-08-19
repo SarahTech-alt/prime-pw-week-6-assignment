daa0d88aa63abedc558aeffbf501913e38a52c60
Sarah Fuoss <sfuoss@gmail.com>
2021-08-19 09:14:56 -0500

    show second approach on DOM

--- assignment/scripts/client.js
+++ assignment/scripts/client.js
@@ -20,29 +20,34 @@ function newCar(yearInput, makeInput, modelInput){
   return true;
 } // end newCar
 
-
+// once page is finished loading and the button is clicked execute addCar function
 function ready(){
-  $("#addButton").on('click', addCar() );
+  $("#addButton").on('click', addCar );
 }
 
+// executes newCar function with user inputs as parameters
 function addCar(){
-  newCar($("#carYear").val(), $("#carMake").val(), $("#carModel").val());
+  newCar($("#yearInput").val(), $("#makeInput").val(), $("#modelInput").val());
+  //console.log($("#yearInput").val());
+  $("#garageList").append(`<li> ${$("#yearInput").val()} ${$("#makeInput").val()} ${$("#modelInput").val()} </li>`);
 }
 
-// function assign(){
-//   // placing user input into variables to work with
-//   let yearInput = $("#yearInput").val();
-//   let makeInput = $("#makeInput").val();
-//   let modelInput = $("#modelInput").val();
-//   // running the newCar function to add items to garage
-//   newCar(yearInput,makeInput,modelInput);
-//   // add car input to DOM
-//   $("#garageList").append(`<li> ${yearInput} ${makeInput} ${modelInput}`).css('background-color','#cac7a8').css('color','#0f020b');
-//   // clear input values
-//   $("#yearInput").val('');
-//   $("#makeInput").val('');
-//   $('#modelInput').val('');
-// }
+
+// Alternate approach to the problem would call assign on the button click instead of addCar
+function assign(){
+  // placing user input into variables to work with
+  let yearInput = $("#yearInput").val();
+  let makeInput = $("#makeInput").val();
+  let modelInput = $("#modelInput").val();
+  // running the newCar function to add items to garage
+  newCar(yearInput,makeInput,modelInput);
+  // add car input to DOM
+  $("#garageList").append(`<li> ${yearInput} ${makeInput} ${modelInput}`).css('background-color','#cac7a8').css('color','#0f020b');
+  // clear input values
+  $("#yearInput").val('');
+  $("#makeInput").val('');
+  $('#modelInput').val('');
+}
 
 // if (newCar()){
 //   //for (let cars in garage){
